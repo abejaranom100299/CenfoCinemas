93420c5a54a7c7f42a968bad399c7d41d956b8b0
graci <abejaranom@ucenfotec.ac.cr>
2025-06-19 14:10:54 -0600

    Clase de UserCrudFactory y MovieCrudFactory

---
+++ DataAccess/CRUD/CrudFactory.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.DAO;
+using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CRUD
+
+    //Clase padre/madre abstracta de los crud
+    //define como se hacen cruds en la arquitectura
+{
+    public abstract class CrudFactory
+    {
+        protected SqlDao _sqlDao;
+
+
+        //Definir los métodos que forman parte del contrato.
+        //C = Create
+        //R = Retrieve
+        //U = Update
+        //D = Delete
+
+        public abstract void Create(BaseDTO baseDTO);
+        public abstract void Update(BaseDTO baseDTO);
+        public abstract void Delete(BaseDTO baseDTO);
+
+        public abstract T Retrive<T>();
+        public abstract T RetrieveById<T>();
+
+        public abstract List<T> RetrieveAll<T>();
+
+    }
+}
--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -1,27 +1,105 @@
 ﻿
 
+using DataAccess.CRUD;
 using DataAccess.DAO;
+using DTOs;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 public class Program{
 
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args) {
+        Console.WriteLine("Seleccione la opcion deseada: ");
+        Console.WriteLine("1. Crear usuario");
+        Console.WriteLine("2. Consultar usuarios");
+        Console.WriteLine("3. Actualizar usuarios");
+        Console.WriteLine("4. Eliminar usuarios");
+        Console.WriteLine("5. Crear pelicula");
+        Console.WriteLine("6. Consultar peliculas");
+        Console.WriteLine("7. Actualizar peliculas");
+        Console.WriteLine("8. Eliminar peliculas");
+
+        var option = Int32.Parse(Console.ReadLine());
         var sqlOperation = new SqlOperation();
-        sqlOperation.ProcedureName = "CRE_USER_PR";
+       
 
-        sqlOperation.ProcedureName = "CRE_USER_PR";
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Digite el codigo de usuario");
+                var userCode = Console.ReadLine();
+                 
+                Console.WriteLine("Digite el nombre");
+                var name = Console.ReadLine();
 
-        sqlOperation.AddStringParameter("P_UserCode", "abejarano");
-        sqlOperation.AddStringParameter("P_Name", "Ana");
-        sqlOperation.AddStringParameter("P_Email", "abejarano@ucenfotec.ac.cr");
-        sqlOperation.AddStringParameter("P_Password", "Ana123!");
-        sqlOperation.AddStringParameter("P_Status", "AC");
-        sqlOperation.AddDateTimeParam("P_BirthDate", DateTime.Now);
+                Console.WriteLine("Digite el email");
+                var email = Console.ReadLine();
 
+                Console.WriteLine("Digite el password");
+                var password = Console.ReadLine();
+
+                var status = "AC";
+
+                Console.WriteLine("Digite la fecha de nacimiento");
+                var bdate = DateTime.Parse(Console.ReadLine());
+
+                //Creamos el objeto del usuario apartir de los valores capturados en consola.
+                var user = new User()
+                {
+                    UserCode = userCode,
+                    UserName = name,
+                    Email = email,
+                    Password = password,
+                    Status = status,
+                    BirthDate = bdate
+               };
+                var uCrud = new UserCrudFactory();
+                uCrud.Create(user);
+                break;
+
+                case 2:
+                uCrud = new UserCrudFactory();
+                var listUsers = uCrud.RetrieveAll<User>();
+                foreach(var u in listUsers)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(u));
+                }
+                break;
+                case 5:
+
+                Console.WriteLine("Digite el titulo");
+                var title = Console.ReadLine();
+
+                Console.WriteLine("Digite la description");
+                var desc = Console.ReadLine();
+
+                Console.WriteLine("Digite la fecha de lanzamiento");
+                var rDate = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite el genero de la pelicula");
+                var genre = Console.ReadLine();
+
+                Console.WriteLine("Digite el director");
+                var director = Console.ReadLine();
+
+
+
+                sqlOperation.ProcedureName = "CRE_MOVIE_PR";
+                sqlOperation.AddStringParameter("P_Title", title);
+                sqlOperation.AddStringParameter("P_Desc",desc);
+                sqlOperation.AddDateTimeParam("P_ReleaseDate", rDate);
+                sqlOperation.AddStringParameter("P_Password", "Ana123!");
+                sqlOperation.AddStringParameter("P_Genre", genre);
+                sqlOperation.AddStringParameter("P_Director",director);
+
+                
+               break;
+        }
+        //Ejecucion del procedure
         var sqlDao = SqlDao.GetInstance();
+        //sqlDao.ExecuteProcedure(sqlOperation);
 
-        sqlDao.ExecuteProcedure(sqlOperation);
     }
 }
 
